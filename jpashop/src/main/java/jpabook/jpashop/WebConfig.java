283ec25489539b671a92ac75309e92c0f8a94f46
dsad2221s <ghksgml0555@naver.com>
2024-01-08 15:02:07 +0900

    스프링인터셉터(로그인체크) 구현과 등록

---
+++ jpashop/src/main/java/jpabook/jpashop/WebConfig.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/new", "/login","/logout",
+                        "/css/**", "/*.ico", "/error");
+    }
+
+}
